48bf13476bc1e57d12fa2e7aa38320a122a989a9
meryem <myem9333@gmail.com>
2023-07-19 09:25:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef main_h
-#define main_h
+	#define main_h
 
 
 	int _putchar(char);
@@ -14,3 +14,7 @@
 	void times_table(void);
 	int add(int, int);
 	void print_to_98(int n);
+	void print_times_table(int n);
+
+
+	#endif
